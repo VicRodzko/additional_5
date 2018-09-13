8932871620966848ec8031a20e16ea273c02c112
Victor Rodzko <vicrodzko@mail.ru>
2018-09-13 14:48:58 +0300

    open and closed brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+	let configStr;
+
+	for (let k = 0; k < 30; k++) {
+		for (let i = 0; i < bracketsConfig.length; i++) {
+			configStr = bracketsConfig[i].join('');
+			while ("крути педали") {
+				if (str.includes(configStr)) {
+					str = str.replace(configStr, '');
+				} else break;
+			}
+		}
+	}
+
+	if (str.length === 0) {
+		return true;
+  } else return false;
+  
 }
